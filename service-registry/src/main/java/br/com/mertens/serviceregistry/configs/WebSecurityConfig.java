577c3a424da526fc8fb22f3efabceff118fb5891
unknown <gvmertens@gmail.com>
2022-10-10 12:06:51 -0300

    Service Registry 20221010

---
+++ service-registry/src/main/java/br/com/mertens/serviceregistry/configs/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package br.com.mertens.serviceregistry.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class WebSecurityConfig {
+
+    @Value("${ead.serviceRegistry.username}")
+    private String username;
+
+    @Value("${ead.serviceRegistry.password}")
+    private String password;
+
+    @Bean
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin();
+    }
+
+    @Bean
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(username)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
